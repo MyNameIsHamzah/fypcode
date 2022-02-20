19f231d23f6580821b05599c34f8ded877bdfa42
Ryan McGouran <rmcgouran6@gmail.com>
2022-02-20 00:07:32 +0000

    Centring login form

--- reactproject/src/components/App.js
+++ reactproject/src/components/App.js
@@ -1,18 +1,18 @@
-import React from "react"
-import Signup from "./Signup"
-import { Container } from "react-bootstrap"
-import { AuthProvider } from "../contexts/AuthContext"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React from "react";
+import Signup from "./Signup";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //import Switch from "react-router-dom"
 
-import Dashboard from "./Dashboard"
-import Login from "./Login"
-import PrivateRoute from "./PrivateRoute"
-import ForgotPassword from "./ForgotPassword"
-import UpdateProfile from "./UpdateProfile"
-import Profile from "./Profile"
-import BMICalculator from "./BMICalculator"
-import TDEECalculator from "./TDEECalculator"
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
+import Profile from "./Profile";
+import BMICalculator from "./BMICalculator";
+import TDEECalculator from "./TDEECalculator";
 
 function App() {
   return (
@@ -21,20 +21,40 @@ function App() {
         <Router>
           <AuthProvider>
             <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute exact path="/profile" component={Profile} />
-              <PrivateRoute exact path="/BMICalculator" component={BMICalculator} />
-              <PrivateRoute exact path="/TDEECalculator" component={TDEECalculator} />
+              <PrivateRoute
+                exact
+                path="/"
+                component={Dashboard}
+                title="Dashboard"
+              />
+              <PrivateRoute
+                exact
+                path="/profile"
+                component={Profile}
+                title="Profile"
+              />
+              <PrivateRoute
+                exact
+                path="/BMICalculator"
+                component={BMICalculator}
+                title="BMI Calculator"
+              />
+              <PrivateRoute
+                exact
+                path="/TDEECalculator"
+                component={TDEECalculator}
+                title="TDEE Calculator"
+              />
               <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
+              <Route path="/signup" component={Signup} title="Signup" />
+              <Route path="/login" component={Login} title="Login" />
               <Route path="/forgot-password" component={ForgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
       </div>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- reactproject/src/components/Login.js
+++ reactproject/src/components/Login.js
@@ -1,36 +1,45 @@
-import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
-import { Link, useHistory } from "react-router-dom"
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
 
 export default function Login() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const { login } = useAuth()
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
-  const history = useHistory()
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      setError("")
-      setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
-      history.push("/")
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      history.push("/");
     } catch {
-      setError("Failed to log in")
+      setError("Failed to log in");
     }
 
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
     <>
-      <Card>
+      <Card
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          alignSelf: "center",
+          marginTop: "30%",
+          marginLeft: "30%",
+          width: "500px",
+        }}
+      >
         <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
+          <h2 className="text-center mb-4">Login</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
@@ -41,8 +50,12 @@ export default function Login() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Log In
+            <Button
+              disabled={loading}
+              className="w-50 mt-2 align-items-center justify-content-center"
+              type="submit"
+            >
+              Login
             </Button>
           </Form>
           <div className="w-100 text-center mt-3">
@@ -54,5 +67,5 @@ export default function Login() {
         Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
-  )
+  );
 }
